0f087f4e008c6be862e746bd2bfc37a01569ab17
Seth Axen <seth.axen@gmail.com>
2021-02-08 18:23:31 -0800

    Add distplot

--- src/ingredients.jl
+++ src/ingredients.jl
@@ -71,3 +71,14 @@ end
 
 RecipesBase.@shorthands kde2dplot
 
+RecipesBase.@recipe function f(::Type{Val{:distplot}}, x, y, z; kind = :auto)
+    if kind == :hist || all(isinteger, y)
+        seriestype := :barhist
+        normalize --> :pdf
+    else
+        seriestype := :kdeplot
+    end
+    ()
+end
+
+RecipesBase.@shorthands distplot
