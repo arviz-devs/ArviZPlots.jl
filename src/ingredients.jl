eb358fbf17affe741517fa95cd77fce67ca8fe4a
Seth Axen <seth.axen@gmail.com>
2021-02-08 17:13:03 -0800

    Add more config options for rug

--- src/ingredients.jl
+++ src/ingredients.jl
@@ -1,11 +1,24 @@
+const DEFAULT_RUG_SIZE = 8
+
 RecipesBase.@recipe function f(
-    ::Type{Val{:rugplot}}, x, y, z; rugmarkershape=:vline, rugsize=8, rugposition=0
+    ::Type{Val{:rugplot}},
+    x,
+    y,
+    z;
+    rugmarkershape=:vline,
+    rugsize=DEFAULT_RUG_SIZE,
+    rugcolor=get(plotattributes, :seriescolor, :auto),
+    rugalpha=get(plotattributes, :seriesalpha, :auto),
+    rugposition = 0,
 )
     seriestype := :scatter
     x := y
     y := fill(rugposition, length(y))
     markershape := rugmarkershape
     markersize := rugsize
+    markercolor := rugcolor
+    markeralpha := rugalpha
+    fill := false  # necessary in case fill=true passed to parent
     label := ""
     return ()
 end
