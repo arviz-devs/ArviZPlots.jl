91f50bcf49d2f9cf7df29c8f0ad7f9fb438de1a4
Seth Axen <seth.axen@gmail.com>
2021-02-08 17:18:03 -0800

    Add kdeplot

--- src/ingredients.jl
+++ src/ingredients.jl
@@ -1,3 +1,5 @@
+# smaller plot recipes used to make more complicated plot recipes
+
 const DEFAULT_RUG_SIZE = 8
 
 RecipesBase.@recipe function f(
@@ -22,3 +24,37 @@ RecipesBase.@recipe function f(
     label := ""
     return ()
 end
+
+# like `density(x)` from StatsPlots, but we keep the density values around to place the rugplot
+RecipesBase.@recipe function f(
+    ::Type{Val{:kdeplot}}, x, y, z; cumulative=false, showrug=false, rugspace=0.05
+)
+    kd = KernelDensity.kde(y; boundary=extrema(y))
+    ynew = kd.density
+    if cumulative
+        ynew = cumsum(ynew)
+        ynew ./= ynew[end]
+    end
+    if showrug
+        ymax = maximum(ynew)
+        if haskey(plotattributes, :rugsize)
+            rugspace *= plotattributes[:rugsize] / DEFAULT_RUG_SIZE
+        end
+        RecipesBase.@series begin
+            primary := false
+            seriestype := :rugplot
+            rugposition := -rugspace * ymax
+            y := y
+            ()
+        end
+    else # no need to set ylims if rug already widened axes for us
+        ylims --> (0, Inf)
+    end
+    seriestype := :path
+    x := kd.x
+    y := ynew
+    return ()
+end
+
+RecipesBase.@shorthands kdeplot
+
