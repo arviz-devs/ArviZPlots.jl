2cbf6bc7d1227e3a518aa439db3bfa8c29e82d85
Seth Axen <seth.axen@gmail.com>
2021-02-10 14:14:54 -0800

    Allow ecdf in distplot

--- src/distplot.jl
+++ src/distplot.jl
@@ -1,8 +1,8 @@
 """
-    kdeplot(x; kwargs...)
-    kdeplot!(x; kwargs...)
+    distplot(x; kwargs...)
+    distplot!(x; kwargs...)
 
-Plot distribution as histogram or kernel density estimates.
+Plot distribution as histogram, KDE, or ECDF.
 
 By default continuous variables are plotted using KDEs and discrete ones using histograms
 
@@ -11,20 +11,25 @@ By default continuous variables are plotted using KDEs and discrete ones using h
 
 # Keyword Arguments
 - `kind=:auto`: By default (`:auto`), continuous variables are plotted using KDEs and
-    discrete ones using histograms. To override this use `:hist` to plot histograms and `:kde`
-    for KDEs
+    discrete ones using histograms. The following values can override this behavior:
+    + `kind=:kde`: Plot using a kernel density estimate (see [`kdeplot`](@ref))
+    + `kind=:hist`: Plot using a histogram (see [`Plots.histogram`]
+    + `kind=:ecdf`: Plot using an empirical cumulative distribution function
+        (see [`ecdfplot`](@ref))
 - `kwargs`: Additional attributes understood by Plots.jl. In particular, see keyword
     arguments for [`kdeplot`](@ref) and `histogram`.
 """
 RecipesBase.@shorthands distplot
 
 RecipesBase.@recipe function f(::Type{Val{:distplot}}, x, y, z; kind=:auto)
-    if kind == :hist || (kind !== :kde && all(isinteger, y))
+    if kind == :hist || (kind === :auto && all(isinteger, y))
         seriestype := :barhist
         normalize --> :pdf
         orientation := isrotated(plotattributes) ? :horizontal : :vertical
-    else
+    elseif kind === :kde || kind === :auto
         seriestype := :kdeplot
+    elseif kind === :ecdf
+        seriestype := :ecdfplot
     end
     return ()
 end
