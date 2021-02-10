67cf24a4ce0caee15e103e1719d0b531be91339d
Seth Axen <seth.axen@gmail.com>
2021-02-10 11:34:02 -0800

    Dispatch to kde2dplot if needed

--- src/kdeplot.jl
+++ src/kdeplot.jl
@@ -11,13 +11,14 @@ function kde(x, y; kwargs...)
 end
 
 """
-    kdeplot(x; kwargs...)
-    kdeplot!(x; kwargs...)
+    kdeplot(x[, y]; kwargs...)
+    kdeplot!(x[, y]; kwargs...)
 
-Plot a 1D kernel density estimate (KDE) taking into account boundary conditions.
+Plot a 1D or 2D kernel density estimate (KDE) taking into account boundary conditions.
 
 # Positional Arguments
-- `x`: values
+- `x`: Values to plot.
+- `y`: Values to plot. If present, a 2D KDE will be estimated. See [`kde2dplot`](@ref).
 
 # Keyword Arguments
 - `cumulative=false`: If `true`, plot the estimated cumulative distribution function
@@ -25,7 +26,7 @@ Plot a 1D kernel density estimate (KDE) taking into account boundary conditions.
 - `rugspace=$DEFAULT_RUG_SPACE`: control the `y` position of the rugplot. The larger this
     number the lower the rugplot
 - `kwargs`: Additional attributes understood by Plots.jl. In particular, see keyword
-    arguments for [`rugplot`](@ref).
+    arguments for [`kde2dplot`](@ref) and [`rugplot`](@ref).
 """
 RecipesBase.@shorthands kdeplot
 
@@ -38,37 +39,43 @@ RecipesBase.@recipe function f(
     showrug=false,
     rugspace=DEFAULT_RUG_SPACE,
 )
-    isrot = isrotated(plotattributes)
-    xnew, ynew = kde(y)
-    if cumulative
-        ynew = cumsum(ynew)
-        ynew ./= ynew[end]
-    end
-    if showrug
-        ymax = maximum(ynew)
-        if haskey(plotattributes, :rugsize)
-            rugspace *= plotattributes[:rugsize] / DEFAULT_RUG_SIZE
+    if x === eachindex(y) # x was (probably) auto-generated from y
+        isrot = isrotated(plotattributes)
+        xnew, ynew = kde(y)
+        if cumulative
+            ynew = cumsum(ynew)
+            ynew ./= ynew[end]
         end
-        RecipesBase.@series begin
-            primary := false
-            seriestype := :rugplot
-            rugposition := -rugspace * ymax
-            y := y
-            ()
+        if showrug
+            ymax = maximum(ynew)
+            if haskey(plotattributes, :rugsize)
+                rugspace *= plotattributes[:rugsize] / DEFAULT_RUG_SIZE
+            end
+            RecipesBase.@series begin
+                primary := false
+                seriestype := :rugplot
+                rugposition := -rugspace * ymax
+                y := y
+                ()
+            end
+        else # no need to set ylims if rug already widened axes for us
+            if isrot
+                xlims --> (0, Inf)
+            else
+                ylims --> (0, Inf)
+            end
         end
-    else # no need to set ylims if rug already widened axes for us
-        if isrot
-            xlims --> (0, Inf)
-        else
-            ylims --> (0, Inf)
+        seriestype := :path
+        if isrotated(plotattributes)
+            xnew, ynew = ynew, xnew
         end
+        x := xnew
+        y := ynew
+    else
+        seriestype := :kde2dplot
+        x := x
+        y := y
     end
-    seriestype := :path
-    if isrotated(plotattributes)
-        xnew, ynew = ynew, xnew
-    end
-    x := xnew
-    y := ynew
     return ()
 end
 
@@ -83,7 +90,7 @@ Plot a 2D kernel density estimate (KDE) taking into account boundary conditions.
 - `y`: `y` coordinates of the values
 
 # Keyword Arguments
-- `cumulative=true`: If `true`, plot the 2D KDE using contours, otherwise plot a heatmap.
+- `contour=true`: If `true`, plot the 2D KDE using contours, otherwise plot a heatmap.
 - `kwargs`: Additional attributes understood by Plots.jl.
 """
 RecipesBase.@shorthands kde2dplot
