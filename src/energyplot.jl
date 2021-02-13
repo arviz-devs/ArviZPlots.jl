d8198eaed7c2d0bd1af4a2a66ff0826653f9d3b2
Seth Axen <seth.axen@gmail.com>
2021-02-12 17:26:14 -0800

    Add BFMI to legend

--- src/energyplot.jl
+++ src/energyplot.jl
@@ -11,6 +11,8 @@ This may help to diagnose poor exploration by gradient-based algorithms like HMC
     `sample_stats` group with an `energy` variable.
 
 # Keyword Arguments
+- `showbfmi=true`: If `true` add to the plot the value of the estimated Bayesian fraction of
+    missing information.
 - `kwargs`: Additional attributes understood by Plots.jl. In particular, see keyword
     arguments for [`distplot`](@ref).
 """
@@ -19,12 +21,29 @@ RecipesBase.@userplot EnergyPlot
 RecipesBase.@recipe function f(p::EnergyPlot; showbfmi=true)
     data = only(p.args)
     energy = ArviZ.convert_to_dataset(data; group=:sample_stats).energy.values
+
+    if showbfmi
+        bfmi = ArviZ.bfmi(energy)
+        nchains = length(bfmi)
+        labels = ["Chain $i BFMI = $(@sprintf("%.2f",bfmi[i]))" for i in 1:nchains]
+        # hack to just add legend entries
+        # TODO: work out how to do this without incrementing color cycle. Ideally would
+        # use `primary=false`, but this removes the series from the legend
+        RecipesBase.@series begin
+            seriestype := :path
+            label := permutedims(labels)
+            seriesalpha := false
+            seriescolor := :white
+            [[0] for _ in 1:nchains], [[0] for _ in 1:nchains]
+        end
+    end
+
     energy_marginal = vec(energy .- StatsBase.mean(energy))
     energy_trans = vec(diff(energy; dims=2))
     seriestype := distplot
     label := ["Marginal energy" "Energy transition"]
     legend --> :best
-    fill --> true
+    fill --> get(plotattributes, :kind, :default) !== :ecdf
     fillalpha --> [1 0.75]
     grid --> false
     ticks --> nothing
